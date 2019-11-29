2d8526ea844c5f15dd3273b4bf34fc066fdeedf3
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-11-29 14:29:43 +0630

    build project and insert layout for adding vocab

--- app/src/main/java/com/example/vocabularybook/MainActivity.java
+++ app/src/main/java/com/example/vocabularybook/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.vocabularybook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnWriteVocab;
+    Button btnReadVocab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnWriteVocab = (Button) findViewById(R.id.btnWriteVocab);
+        btnReadVocab = (Button) findViewById(R.id.btnReadVocab);
+
+        btnWriteVocab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent readVocabIntent = new Intent(MainActivity.this,ReadVocabActivity.class);
+                startActivity(readVocabIntent);
+            }
+        });
+
+        btnReadVocab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent writeVocabIntent = new Intent(MainActivity.this,WriteVocabActivity.class);
+                startActivity(writeVocabIntent);
+            }
+        });
     }
 }
